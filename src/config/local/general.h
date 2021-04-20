137c63fae5bd962d79dea2eef81001c701618fb1
Andrew St Clair <andrew_stclair95@hotmail.com>
2021-04-21 00:17:46 +1000

    Add src/config/local files

---
+++ src/config/local/general.h
@@ -0,0 +1,4 @@
+#define CONSOLE_CMD		        /* Console command */
+#define DOWNLOAD_PROTO_HTTPS    /* Secure Hypertext Transfer Protocol */
+#define NTP_CMD		            /* NTP commands */
+#define TIME_CMD                /* Time commands */
